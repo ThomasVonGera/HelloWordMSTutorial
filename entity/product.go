fd83ec08f07bb38119fafb736290f5d9631793e5
ThomasVonGera <32611351+ThomasVonGera@users.noreply.github.com>
2021-08-09 12:25:51 +0200

    initialisierung

---
+++ entity/product.go
@@ -0,0 +1,9 @@
+package entity
+
+type Product struct {
+	ID          string  `json:"id"`
+	Name        string  `json:"name"`
+	Description string  `json:"description"`
+	Price       float64 `json:"price"`
+	IsAvaiable  bool    `json:"isAvaiable"`
+}
---
+++ handlers/handlers.go
@@ -0,0 +1,11 @@
+package handlers
+
+import {
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"os"
+
+	
+}
+
